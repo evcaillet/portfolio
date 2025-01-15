cb1be10434ee3562b61d0f3e3bd605d11d944065
evcaillet <159443886+evcaillet@users.noreply.github.com>
2025-01-15 09:34:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const maxWidth = 550;
+const fixedPercentageReseaux = 70;
+const fixedPercentageTelec = 75;
+const fixedPercentageProg = 90;
 
-function updateGaugeReseaux(value) {
-    const fill = document.querySelector('.gauge-fill-reseaux');
-    const label = document.querySelector('.gauge-label-reseaux');
+function updateGauge(value, jaugefill, jaugelabel) {
+    const fill = document.querySelector(jaugefill);
+    const label = document.querySelector(jaugelabel);
 
     // Limiter la valeur entre 0 et 100
     const percentage = Math.max(0, Math.min(value, 100));
@@ -13,41 +16,9 @@ function updateGaugeReseaux(value) {
     label.textContent = `${percentage}%`;
   }
 
-const fixedPercentageReseaux = 70;
-
-function updateGaugeTelec(value) {
-  const fill = document.querySelector('.gauge-fill-telec');
-  const label = document.querySelector('.gauge-label-telec');
-
-  // Limiter la valeur entre 0 et 100
-  const percentage = Math.max(0, Math.min(value, 100));
-  
-  // Mettre à jour la largeur du remplissage et le texte
-  const width = (percentage / 100) * maxWidth;
-  fill.setAttribute('width', width);
-  label.textContent = `${percentage}%`;
-}
-
-const fixedPercentageTelec = 75;
-
-function updateGaugeProg(value) {
-  const fill = document.querySelector('.gauge-fill-prog');
-  const label = document.querySelector('.gauge-label-prog');
-
-  // Limiter la valeur entre 0 et 100
-  const percentage = Math.max(0, Math.min(value, 100));
-  
-  // Mettre à jour la largeur du remplissage et le texte
-  const width = (percentage / 100) * maxWidth;
-  fill.setAttribute('width', width);
-  label.textContent = `${percentage}%`;
-}
-
-const fixedPercentageProg = 90;
-
 // Appeler la fonction après que le DOM soit chargé
 document.addEventListener("DOMContentLoaded", () => {
-  updateGaugeReseaux(fixedPercentageReseaux);
-  updateGaugeTelec(fixedPercentageTelec);
-  updateGaugeProg(fixedPercentageProg);
+  updateGauge(fixedPercentageReseaux, '.gauge-fill-reseaux', '.gauge-label-reseaux');
+  updateGauge(fixedPercentageTelec, '.gauge-fill-telec', '.gauge-label-telec');
+  updateGauge(fixedPercentageProg, '.gauge-fill-prog', '.gauge-label-prog');
 });
